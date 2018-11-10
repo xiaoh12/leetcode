53b58499392f09a25ca9719c8ff08bbca1426d94
chenhui <hui.chen6789@gmail.com>
2018-11-10 15:53:59 +0800

    add 13. Roman to Integer

---
+++ 13+Roman+to+Integer/README.md
@@ -0,0 +1,5 @@
+# 题目链接：https://leetcode.com/problems/roman-to-integer/description/
+
+## 1. 算法
+
+这题比较简单，判断好4，9的特殊情况就好了
---
+++ 13+Roman+to+Integer/alg.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+#-*- coding: UTF-8 -*-
+###########################################################################
+#
+# Copyright (c) 2018 Baidu.com, Inc. All Rights Reserved
+#
+##########################################################################
+'''
+ @brief leetcode algorithm
+ @author chenhui(hui.chen6789@gmail.com)
+ @date 2018/11/08 19:09:33
+'''
+
+class Solution:
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        dict_roman = {
+           'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000 
+        }
+        num, i = 0, 0
+        while i < len(s):
+            if i+1<len(s) and dict_roman[s[i+1]] > dict_roman[s[i]]:
+                num += dict_roman[s[i+1]]-dict_roman[s[i]]
+                i +=2
+            else :
+                num += dict_roman[s[i]]
+                i +=1
+        return num  
+
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.romanToInt('III'))
+    print(s.romanToInt('III'))
