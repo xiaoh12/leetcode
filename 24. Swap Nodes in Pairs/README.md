cf0e474091fddf72a0fb8d958d579f4a0dd92c34
chenhui <hui.chen6789@gmail.com>
2018-11-27 19:40:27 +0800

    add 24. Swap Nodes in Pairs

---
+++ 24. Swap Nodes in Pairs/README.md
@@ -0,0 +1,4 @@
+# 题目链接：https://leetcode.com/problems/swap-nodes-in-pairs/description/
+
+## 算法
+这题只要能写出链表反转，应该也不会有什么问题，只要理清楚中间赋值的过程就可以了
---
+++ 24. Swap Nodes in Pairs/alg.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+#-*- coding: UTF-8 -*-
+###########################################################################
+#
+# Copyright (c) 2018 www.codingchen.com, Inc. All Rights Reserved
+#
+##########################################################################
+'''
+  @brief leetcode algorithm
+  @author chenhui
+  @date 2018-11-16 23:15:11
+'''
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def swapPairs(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        # Given 1->2->3->4, you should return the list as 2->1->4->3.
+        pre, pre.next = self, head 
+        while pre.next and pre.next.next:
+            a = pre.next
+            b = a.next
+            pre.next, b.next, a.next = b, a, b.next
+            pre = a
+        return self.next
+
